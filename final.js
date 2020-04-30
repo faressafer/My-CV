785e24bd52524249c21e8925a10603638bcaec63
faressafer <34158695+faressafer@users.noreply.github.com>
2020-04-30 05:14:38 +0100

    first commit

---
+++ final.js
@@ -0,0 +1,97 @@
+/**
+ * moving site description 
+ * 
+ */
+var textWrapper = document.querySelector('.ml10 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml10 .letter',
+    rotateY: [-90, 0],
+    duration: 1300,
+    delay: (el, i) => 45 * i
+  }).add({
+    targets: '.ml10',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+  /**
+ * moving site description 
+ * 
+ */
+  /**
+ * form js beginn 
+ * 
+ */
+
+  /**
+ * form endddddd
+ * 
+ */
+ /**
+ * section 2 js
+ * 
+ */
+const form = document.querySelector('.signin');
+const errorBlock = document.querySelector('.error-block');
+const errorText = document.querySelector('.error');
+const username = document.querySelector('.username-input');
+const email = document.querySelector('.email-input');
+const password = document.querySelector('.pw-input');
+
+
+const fields = [username, email, password];
+const usersFromDB = ['test', 'test123', 'halelujah'];
+
+const throwError = message => {
+	errorBlock.style.display = 'block';
+	errorText.innerHTML = message;
+};
+const emptyFields = fields => {
+	let isEmpty;
+	fields.forEach(field =>
+		field.value.length === 0 ? (isEmpty = true) : (isEmpty = false)
+	);
+	return isEmpty;
+};
+const uniqueUser = (usersFromDB, username) => {
+	if (!usersFromDB.indexOf(username.value)) return false;
+	else return true;
+};
+const validEmail = email => {
+	const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return emailRegex.test(email.value) ? true : false;
+};
+const validPassowrd = password => {
+	if (password.value.length < 6) return false;
+	else return true;
+};
+
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	if (emptyFields(fields)) throwError('All fields must be completed.');
+	else if (!uniqueUser(usersFromDB, username)) throwError('Username taken.');
+	else if (!validEmail(email)) throwError('You must enter a valid email.');
+	else if (!validPassowrd(password))
+		throwError('Your password must have at least 6 characters.');
+	else {
+		errorBlock.classList.add('success');
+		throwError('Submitted.');
+	}
+});
+
+ /**
+ * section 2
+ * 
+ */
+
+$(document).ready(function(){
+  $(".contactBtn").click(function(){
+    $(".landing").hide();
+    $(".signin").show()
+  });
+});
